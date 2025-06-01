872647be010d241b60867d1bbf92ce21900aff39
Brabyns Yabwetsa <burabuyabz@gmail.com>
2025-06-01 21:16:42 +0300

    first commit

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div >
+      <h1 className="text-red-950 font-black">Hello World</h1>
+        <Button>Click</Button>
+    </div>
+  );
+}

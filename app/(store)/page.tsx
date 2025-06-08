adf55a9cebffab615c3c068e17bfb409f2c4f6a8
Brabyns Yabwetsa <burabuyabz@gmail.com>
2025-06-08 21:08:42 +0300

    Implememnt the Card

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,11 +1,23 @@
-import {Button} from "@/components/ui/button";
+
+import {getAllProducts} from "@/sanity/lib/products/getAllProducts";
+import ProductsView from "@/components/ProductsView";
+import {getAllCategories} from "@/sanity/lib/products/getAllCategories";
 
 
-export default function Home() {
+export default async function Home() {
+    const products = await getAllProducts()
+    const categories = await getAllCategories()
+    // console.log(
+    //     cypto.randomUUID().slice(0, 5) +
+    //     `>>> Rerendered the home cache with ${products.length} products and ${categories.length} categories.`
+    // )
   return (
     <div >
       <h1 className="text-red-950 font-black">Hello World</h1>
-        <Button>Click</Button>
+        <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+            <ProductsView products={products} />
+        </div>
+
     </div>
   );
 }
